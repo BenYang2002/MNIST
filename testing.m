bc5028bb86d125f9e00b6268a8a97f0ce318db82
BenYang2002 <96927991+BenYang2002@users.noreply.github.com>
2024-02-19 17:38:15 -0800

    Update testing.m
    testing q1

--- testing.m
+++ testing.m
@@ -1,7 +1,14 @@
-output = [1;1;1];
-input = [1;1];
-obj = BackPropLayer(3,2,3,3,output,0.1,"sigmoid")
-obj.forward(input);
-disp(obj.layers{1});
-obj.backwardUpdate(input);
-disp(obj.layers{1});
+% input for 0
+input0 = [-1,1,1,1,-1,1,-1,-1,-1,1,1,-1,-1,-1,1,1,-1,-1,-1,1,1,-1,-1,-1,1,-1,1,1,1,-1]';
+% input for 1
+input1 = [-1,1,1,-1,-1,-1,-1,1,-1,-1,-1,-1,1,-1,-1,-1,-1,1,-1,-1,-1,-1,1,-1,-1,-1,-1,1,-1,-1]';
+% input for 2
+input2 = [1,1,1,-1,-1,-1,-1,-1,1,-1,-1,-1,-1,1,-1,-1,1,1,-1,-1,-1,1,-1,-1,-1,-1,1,1,1,1]';
+%output matrix 
+output_matrix = [1,0,0;0,1,0;0,0,1];
+input_matrix = [input0,input1,input2];
+
+% testing for correctness
+obj_sample = BackPropLayer(6,30,3,6,0.01,"sigmoid");
+obj_sample.train(input_matrix,output_matrix);
+% testing to find out best hidden neuron numbers
